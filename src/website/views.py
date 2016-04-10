58c0a889c2648733e61a9445f5ca37799e730b4c
Abhijith Anilkumar <abhijithanilkumar@live.com>
2016-04-10 19:47:07 +0530

    Added searchlist page

--- src/website/views.py
+++ src/website/views.py
@@ -7,7 +7,12 @@ def searchbusPage(request):
     if request.method == 'GET':
         return render(request,'searchbus.html')
     if request.method == 'POST':
-        return render(request,'searchbus.html')
+        source = request.POST.get('source','')
+        destination = request.POST.get('destination','')
+        date = request.POST.get('date','')
+        time = request.POST.get('time','')
+        return render(request,'buslist.html')
 
 def searchcarPage(request):
-    return render(request,'searchcar.html')
+    if request.method == 'GET':
+        return render(request,'searchcar.html')
